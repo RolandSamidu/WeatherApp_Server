0b45a4db1965e5063e655618eab800d7ac7bed03
RolandSamidu <samidu.appuhamy@gmail.com>
2024-07-13 03:41:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const connectDB = require('./db/connect');
-// const cron = require('node-cron');
-// const weatherService = require('./Services/weatherService');
-// const emailService = require('./Services/emailService');
-// const User = require('./models/userModel');
+const cron = require('node-cron');
+const weatherService = require('./Services/weatherService');
+const emailService = require('./Services/emailService');
+const User = require('./models/userModel');
 
 const app = express();
 
@@ -20,15 +20,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Schedule the cron job to send emails every 3 hours
-// cron.schedule('0 */3 * * *', async () => {
-//     try {
-//         const users = await User.find();
-//         for (let user of users) {
-//             const weatherData = await weatherService.getWeatherData(user.location);
-//             const weatherText = `Current weather in ${user.location}: ${weatherData.weather[0].description}`;
-//             emailService.sendEmail(user.email, '3 Hourly Weather Report', weatherText);
-//         }
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// });
+cron.schedule('0 */3 * * *', async () => {
+    try {
+        const users = await User.find();
+        for (let user of users) {
+            const weatherData = await weatherService.getWeatherData(user.location);
+            const weatherText = `Current weather in ${user.location}: ${weatherData.weather[0].description}`;
+            emailService.sendEmail(user.email, '3 Hourly Weather Report', weatherText);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+});
